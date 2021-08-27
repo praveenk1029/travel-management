480daf1468819429d50911dde0e1e1eb330f8b23
praveenk1029 <praveenk1029@gmail.com>
2021-08-26 22:57:12 -0400

    Travel Management files - initial version added.

---
+++ src/test/java/com/gpk/travelmanagement/TravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpk.travelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
